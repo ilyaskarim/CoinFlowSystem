f2bc2b1569101bfb27ba714ba9bde5212c855bc9
Ilyas Karim <ilyas.datoo@gmail.com>
2024-11-13 06:52:12 +0500

    Test data

--- tests/test-data/data.ts
+++ tests/test-data/data.ts
@@ -8,25 +8,25 @@ export const users = [
     email: "user_1@example.com",
   },
   {
-    id: uuidv4(),
+    id: "b680a082-b999-4fed-871c-4a194b5710ae",
     username: "user_2",
     application_user_id: "131",
     email: "user_2@example.com",
   },
   {
-    id: uuidv4(),
+    id: "b680a082-b999-4fed-871c-4a194b5710af",
     username: "user_3",
     application_user_id: "167890",
     email: "user_3@example.com",
   },
   {
-    id: uuidv4(),
+    id: "b680a082-b999-4fed-871c-4a194b5710b0",
     username: "user_4",
     application_user_id: "12345123167890",
     email: "user_4@example.com",
   },
   {
-    id: uuidv4(),
+    id: "b680a082-b999-4fed-871c-4a194b5710b1",
     username: "user_5",
     application_user_id: "1231",
     email: "user_5@example.com",
@@ -35,28 +35,28 @@ export const users = [
 
 export const coins = [
   {
-    id: uuidv4(),
+    id: "b680a082-b999-4fed-871c-4a194b5710b2",
     name: "UC",
   },
   {
-    id: uuidv4(),
+    id: "b680a082-b999-4fed-871c-4a194b5710b3",
     name: "USDT",
   },
   {
-    id: uuidv4(),
+    id: "b680a082-b999-4fed-871c-4a194b5710b4",
     name: "USDC",
   },
 ];
 
 export const coinRates = [
   {
-    id: uuidv4(),
+    id: "b680a082-b999-4fed-871c-4a194b5710b5",
     coin_one_id: "UC",
     coin_two_id: "USDT",
     rate: 1,
   },
   {
-    id: uuidv4(),
+    id: "b680a082-b999-4fed-871c-4a194b5710b6",
     coin_one_id: "UC",
     coin_two_id: "USDC",
     rate: 2,
